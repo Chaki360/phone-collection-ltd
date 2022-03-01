b3aa8ff2c105ddf99edc68dde77de558bea746d3
Badhon chaki <fboy4069@gmail.com>
2022-03-01 21:40:47 +0600

    dynamic image added

--- JS/app.js
+++ JS/app.js
@@ -13,21 +13,27 @@ const searchPhone = () => {
 const showSearchResult = data => {
 
     const searchResult = document.getElementById('search-result');
+
     data.forEach(data => {
 
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `<div class="card border-1" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${data.image}" class="w-75 card-img-top" alt="...">
         <div class="card-body">
-            <h3 class="card-title">Name:</h3>
-            <h4 class="card-title">Brand:</h4>
-            <a href="#" class="btn rounded-3 btn-primary fw-bold ">More Info</a>
+            <h3 class="card-title">Name:${data.phone_name}</h3>
+            <h4 class="card-title">Brand:${data.brand}</h4>
+            <a href="#" class="btn rounded-3 btn-primary fw-bold" onclick="phoneDetails()">More Info</a>
         </div>
     </div>
 </div>`
 
         searchResult.appendChild(div);
+
     });
 
+}
+
+const phoneDetails = () => {
+    console.log("ok fine i gotcha")
 }
