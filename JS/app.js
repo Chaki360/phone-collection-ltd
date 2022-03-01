1fd7f3a72b322a5a776b7201d354652a3ad86fab
Badhon chaki <fboy4069@gmail.com>
2022-03-01 19:26:57 +0600

    search result added

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,33 @@
-const allMobiles = () => {
-    const searchValue = document.getElementById('search-field').value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    searchField.value = "";
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
     fetch(url)
         .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((data) => showSearchResult(data.data));
+
+
+}
+const showSearchResult = data => {
+
+    const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `<div class="card border-1" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h3 class="card-title">Name:</h3>
+            <h4 class="card-title">Brand:</h4>
+            <a href="#" class="btn rounded-3 btn-primary fw-bold ">More Info</a>
+        </div>
+    </div>
+</div>`
+
+        searchResult.appendChild(div);
+    });
+
 }
